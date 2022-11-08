4a98487fe0e64b0a061a3eca34a5bc631d664ef1
Simone-De-Santis <simone.agenmtelazio@yahoo.it>
2022-11-08 11:04:25 +0100

    add tax id regex

--- script.js
+++ script.js
@@ -163,8 +163,78 @@ $(document).ready(function () {
 			XK: /\d{5}/gm,
 			YT: /976\d{2}/gm,
 		},
-		italy: {
-			localization: "italy",
+		taxIdRegex: {
+			AL: /\^[J|K][0-9]{8}{L]/gm,
+			AR: /\^[0-9]{11}/gm,
+			AU: /\^[0-9]{11}/gm,
+			AT: /\^ATU[0-9]{8}/gm,
+			BY: /\^[0-9]{9}/gm,
+			BE: /\^BE0[0-9]{9}/gm,
+			BO: /\^[0-9]{7}/gm,
+			BR: /\^[0-9]{8}/gm,
+			BG: /\^BG[0-9]{9,10}/gm,
+			CA: /\^[0-9A-Z]{8,9}/gm,
+			CL: /\^[0-9]{8}/gm,
+			CN: /\^[0-9]{18}/gm,
+			CO: /\^[0-9]{9}/gm,
+			CR: /\^[0-9]{10,12}/gm,
+			HR: /\^HR\d{11}/gm,
+			CY: /\^CY[0-9]{8,9}L/gm,
+			CZ: /\^CZ[0-9]{8,10}/gm,
+			DK: /\^DK[0-9]{8}/gm,
+			DO: /\^[0-9]{9}/gm,
+			EC: /\^[0-9]{13}/gm,
+			SV: /\^[0-9]{14}/gm,
+			EE: /\^EE[0-9]{9}/gm,
+			FI: /\^FI[0-9]{8}/gm,
+			FR: /\^FR[0-9A-Z]{2}[0-9]{9}/gm,
+			DE: /\^DE[0-9]{9}/gm,
+			GR: /\^(EL|GR)[0-9]{9}/gm,
+			GT: /\^[0-9]{9}/gm,
+			HN: /\^[0-9]{14}/gm,
+			HK: /\^[0-9]{8}/gm,
+			HU: /\^HU[0-9]{8}/gm,
+			IS: /\^[0-9][A-Z]{6}/gm,
+			ID: /\^[0-9]{15}/gm,
+			IN: /\^[0-9]{10}{V|C}{1}/gm,
+			IE: /\^IE[0-9]S[0-9]{5}L/gm,
+			IL: /\^[0-9]{9}/gm,
+			IT: /\^IT[0-9]{11}/gm,
+			JP: /\^[0-9]{12,13}/gm,
+			LV: /\^LV[0-9]{11}/gm,
+			LT: /\^LT([0-9]{9}|[0-9]{12})/gm,
+			LU: /\^(LU)?[0-9]{8}/gm,
+			MT: /\^MT[0-9]{8}/gm,
+			MX: /\^([A-ZÑ\x26]{3,4}([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|1[0-9]:|2[0-9]|3[0-1])[A-Z|\d]{3})/gm,
+			NL: /\^NL[0-9]{9}B[0-9]{2}/gm,
+			NZ: /\^[0-9]{13}/gm,
+			NI: /\^([0-9]{13}([A-Z]{1})/gm,
+			NO: /\^MVA[0-9]{9}/gm,
+			PA: /\^[0-9]{14}/gm,
+			PY: /\^[0-9]{7}/gm,
+			PE: /\^[0-9]{11}/gm,
+			PH: /\^[0-9]{12}/gm,
+			PL: /\^PL[0-9]{10}/gm,
+			PT: /\^PT[0-9]{9}/gm,
+			RO: /\^(RO)?[0-9]{2,10}/gm,
+			RU: /\^[0-9]{10,12}/gm,
+			SM: /\^[0-9]{5}/gm,
+			RS: /\^[0-9]{9}/gm,
+			SK: /\^SK[0-9]{10}/gm,
+			SI: /\^SI[0-9]{8}/gm,
+			ES: /\^ES[0-9A-Z][0-9]{7}[0-9A-Z]/gm,
+			SE: /\^SE[0-9]{12}/gm,
+			CH: /\^CHE[0-9]{6,9}/gm,
+			TR: /\^[0-9]{10}/gm,
+			UA: /\^[0-9]{10}/gm,
+			GB: /\^GB([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})/gm,
+			UY: /\^[0-9]{12}/gm,
+			US: /\^\d{2}-\d{7}/gm,
+			UZ: /\^[2|4][0000000}[X][2|4][0-9]{7)[X]/gm,
+			VE: /\^[J|K|V|E]{9}/gm
+		},
+		IT: {
+			localization: "IT",
 			prefixInternational: ["+39", "0039"],
 			cellular: {
 				minLength: 6,
@@ -457,8 +527,8 @@ $(document).ready(function () {
 				],
 			}
 		},
-		spagna: {
-			localization: "italy",
+		ES: {
+			localization: "ES",
 			prefixInternational: ["+38", "0038"],
 			cellular: {
 				minLength: 6,
@@ -918,11 +988,16 @@ $(document).ready(function () {
 					isValid = validazioneEmail(items.value.trim())
 				}
 				// ! VALIDAZIONE postalcode
+				//^ per il momento il postal code dipende dalla selezione della regione nel form
 				if (items.getAttribute("data-validate-type") == 'postalCode') {
 					isValid = checkPostalCode(items.value.trim(), $(".form #selectRegion").val())
 				}
+				// ! validazione identificativo fiscale (P.IVA)
+				if (items.getAttribute("data-validate-type") == 'tax-identification') {
+					isValid =
+				}
 			}
-			//^ cambiamo la classe sul front end in basse all ckeck della funzione
+			//^ cambiamo la classe sul front end in basse all check della funzione utilizzando bootstrap
 			$(items).removeClass('is-valid').removeClass('is-invalid').addClass(isValid ? 'is-valid' : 'is-invalid')
 		});
 	});
