dbabee38e97bbb5952239052fa95ada1d74d1c6f
Simone-De-Santis <simone.agenmtelazio@yahoo.it>
2022-11-09 16:59:58 +0100

    add validator 2

--- class/classValidator2.js
+++ class/classValidator2.js
@@ -211,3 +211,59 @@ export default class Validator {
 
 
 }
+
+
+
+// var esempioClasse = function (params) {
+
+//     let param_x = 1;
+
+//     let param_y = {
+//         1: "a",
+//         2: "b"
+//     };
+
+//     this.init = function (param_1) {
+
+//         console.log(param_1);
+
+//         // todo: Add try catch statement
+//         // Popolo la struttura dello shipment con i dati  ricevuti
+//         $.extend(params);
+
+//         this.funzione("test");
+
+//     };
+
+//     this.funzione = function (foo) {
+
+//         //.....
+
+//     }
+
+
+//     this.init(params);
+//     return this;
+// };
+
+
+
+
+// $(document).ready(function () {
+
+//     let param_1 = {
+//         john: "doe",
+//         foo: "bar"
+//     };
+
+//     var form_ship = new esempioClasse(
+//         param_1
+//     );
+
+// });
+
+
+
+
+
+
